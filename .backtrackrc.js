2364835316739ca71b19072564877e151c900818
Chris Blossom <cbloss@gmail.com>
2018-05-17 16:41:41 -0700

    use backtrack config file

---
+++ .backtrackrc.js
@@ -0,0 +1,21 @@
+'use strict';
+
+module.exports = {
+    presets: ['./'],
+
+    // config: {
+    //     wallaby: (config) => {
+    //         const files = config.files.map((file) => {
+    //             if (file === 'lib/**/*.js') {
+    //                 return { pattern: 'lib/**/*.js', instrument: false };
+    //             }
+    //
+    //             return file;
+    //         });
+    //
+    //         config.files = files;
+    //
+    //         return config;
+    //     },
+    // },
+};
