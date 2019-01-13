b163c11faf7be23337c58c1aa880e124fb28bddb
Chris Blossom <cbloss@gmail.com>
2019-01-12 19:25:13 -0800

    use preset-node-module as base

--- .backtrackrc.js
+++ .backtrackrc.js
@@ -2,20 +2,4 @@
 
 module.exports = {
     presets: ['./'],
-
-    // config: {
-    //     wallaby: (config) => {
-    //         const files = config.files.map((file) => {
-    //             if (file === 'lib/**/*.js') {
-    //                 return { pattern: 'lib/**/*.js', instrument: false };
-    //             }
-    //
-    //             return file;
-    //         });
-    //
-    //         config.files = files;
-    //
-    //         return config;
-    //     },
-    // },
 };
